c9aa0b8d889d012521f3a8fb1ce056e4c7eab763
Richard Crichlow <richardcrichlow@ac.c4q.nyc>
2018-02-21 17:49:30 -0500

    Files Set Up for Firebase experimentation

---
+++ FlashCardAppFeaturingBigShaq/Services/Database/DatabaseService+Add.swift
@@ -0,0 +1,9 @@
+//
+//  DatabaseService+Add.swift
+//  FlashCardAppFeaturingBigShaq
+//
+//  Created by Richard Crichlow on 2/21/18.
+//  Copyright © 2018 Richard Crichlow. All rights reserved.
+//
+
+import Foundation
---
+++ FlashCardAppFeaturingBigShaq/Services/Database/DatabaseServiceDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  DatabaseServiceDelegate.swift
+//  FlashCardAppFeaturingBigShaq
+//
+//  Created by Richard Crichlow on 2/21/18.
+//  Copyright © 2018 Richard Crichlow. All rights reserved.
+//
+
+import Foundation
